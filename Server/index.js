a871472f682c8ad95e7ebcd408c7a6d5bab73d55
sriharireddy-tony <sriharireddyp0143@gmail.com>
2023-11-22 06:49:19 -0800

    22 commit

---
+++ Server/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: '*' }));
+
+//MongoDB Node.js Driver version 3.6.0 
+// If you use these in version 4.0.0 this will deprecate
+const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log('Mongoose connected to db');
+    app.listen(port, () =>
+    console.log(`App listening on port ${port}!`)
+)
+}).catch((err) => console.error('Failed to connect to MongoDB', err));
+
+// mongoose.connection.on('connected', ()=>{
+//     console.log('Mongoose connected to db')
+// });
+
+// mongoose.connection.on('disconnected',()=>{
+// console.log('Mongoose connection is disconnected.')
+// });
+
+app.use('/', (req,res)=>{
+    res.json('Welcome to nodeJS ..!');
+});
