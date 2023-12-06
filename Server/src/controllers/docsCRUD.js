ddabfa17ba322570dda1ec5921d5bfd6d8b3758c
sriharireddy-tony <116810756+sriharireddy-tony@users.noreply.github.com>
2023-12-06 18:53:30 +0530

    commit

--- Server/src/controllers/docsCRUD.js
+++ Server/src/controllers/docsCRUD.js
@@ -1,16 +1,37 @@
 const uploadFile = require('../middlewares/upload');
 const multer = require("multer");
 
-const uploadDocument = (req, res) => {
+const uploadDocument = async(req, res) => {
 
-    uploadFile(req, res, (err) => {
-      if (err instanceof multer.MulterError) {
-        return res.status(400).json({ error: 'Multer error: ' + err.message });
-      } else if (err) {
-        return res.status(500).json({ error: 'Internal server error' });
+   
+
+
+    const storage = multer.diskStorage({
+        destination: (req, file, cb) => {
+          cb(null, "uploads/")
+        },
+        filename: (req, file, cb) => {
+          cb(null, Date.now() + "-" + file.originalname)
+        },
+      })
+
+      const uploadStorage = multer({ storage: storage });
+
+
+      uploadStorage.single("file"), (req, res) => {
+        console.log(req.file)
+        return res.send("Single file")
       }
-      return res.status(200).json({ message: 'File uploaded successfully' });
-    });
+      
+
+    // await uploadFile(req, res, function(err) {
+    //   if (err instanceof multer.MulterError) {
+    //     return res.status(400).json({ error: 'Multer error: ' + err.message });
+    //   } else if (err) {
+    //     return res.status(500).json({ error: 'Internal server error' });
+    //   }
+    //   return res.status(200).json({ message: 'File uploaded successfully' });
+    // });
   };
 
   module.exports = {
--- Server/src/middlewares/upload.js
+++ Server/src/middlewares/upload.js
@@ -1,9 +1,9 @@
 const multer = require("multer");
-const maxSize = 2 * 1024 * 1024;
+const maxSize = 10 * 1024 * 1024;
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "../assets/docs");
+    cb(null, "../assets/docs/");
   },
   filename: (req, file, cb) => {
     console.log(file.originalname);
@@ -11,9 +11,10 @@ let storage = multer.diskStorage({
   },
 });
 
-let uploadFile = multer({
-  storage: storage,
-  limits: { fileSize: maxSize },
-});
+var upload = multer({  
+    storage: storage, 
+    limits: { fileSize: maxSize }, 
 
-module.exports = uploadFile;
+}); 
+
+module.exports = upload;
